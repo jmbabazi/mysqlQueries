e9db1c2cc3b2535eebd45dc0de44babf2bab684d
James Mbabazi <jmbabazi@pih.org>
2016-12-18 17:30:53 -0500

    Create patientsByCategories.sql

---
+++ patientsByCategories.sql
@@ -0,0 +1,35 @@
+-- Returns ID for patients who are XDR, Pre-XDR - fluoroquinolone and Pre-XDR - Injectable --
+SELECT 
+    GROUP_CONCAT(IF(obs.value_coded = 328,
+            (SELECT 
+                    patient_identifier.identifier
+                FROM
+                    patient_identifier
+                WHERE
+                    patient_identifier.patient_id = obs.person_id
+                        AND obs.value_coded = 328),
+            NULL)) AS 'XDR (resistance to a fluoroquinolone and at least one injectable)',
+    GROUP_CONCAT(IF(obs.value_coded = 329,
+            (SELECT 
+                    patient_identifier.identifier
+                FROM
+                    patient_identifier
+                WHERE
+                    patient_identifier.patient_id = obs.person_id
+                        AND obs.value_coded = 329),
+            NULL)) AS 'Pre-XDR - fluoroquinolone (resistance to a fluoroquinolone, but susceptible to second-line injectables)',
+    GROUP_CONCAT(IF(obs.value_coded = 330,
+            (SELECT 
+                    patient_identifier.identifier
+                FROM
+                    patient_identifier
+                WHERE
+                    patient_identifier.patient_id = obs.person_id
+                        AND obs.value_coded = 330),
+            NULL)) AS 'Pre-XDR - injectable (resistance to at least one second-line injectable, but susceptible to a fluoroquinolone'
+FROM
+    obs
+        JOIN
+    patient_identifier ON patient_identifier.patient_id = obs.person_id
+GROUP BY patient_identifier.identifier
+ORDER BY patient_identifier.identifier;
