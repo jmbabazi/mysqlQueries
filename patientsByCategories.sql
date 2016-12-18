c7da5ac5806f1045f5c8282a9d0c1906dfe9b932
James Mbabazi <jmbabazi@pih.org>
2016-12-18 18:08:15 -0500

    Update patientsByCategories.sql

--- patientsByCategories.sql
+++ patientsByCategories.sql
@@ -7,7 +7,7 @@ SELECT
                     patient_identifier
                 WHERE
                     patient_identifier.patient_id = obs.person_id
-                        AND obs.value_coded = 328),
+                        AND obs.value_coded = 328 and obs.voided=0),
             NULL)) AS 'XDR (resistance to a fluoroquinolone and at least one injectable)',
     GROUP_CONCAT(IF(obs.value_coded = 329,
             (SELECT 
@@ -16,7 +16,7 @@ SELECT
                     patient_identifier
                 WHERE
                     patient_identifier.patient_id = obs.person_id
-                        AND obs.value_coded = 329),
+                        AND obs.value_coded = 329 and obs.voided=0),
             NULL)) AS 'Pre-XDR - fluoroquinolone (resistance to a fluoroquinolone, but susceptible to second-line injectables)',
     GROUP_CONCAT(IF(obs.value_coded = 330,
             (SELECT 
@@ -25,7 +25,7 @@ SELECT
                     patient_identifier
                 WHERE
                     patient_identifier.patient_id = obs.person_id
-                        AND obs.value_coded = 330),
+                        AND obs.value_coded = 330 and obs.voided=0),
             NULL)) AS 'Pre-XDR - injectable (resistance to at least one second-line injectable, but susceptible to a fluoroquinolone'
 FROM
     obs
