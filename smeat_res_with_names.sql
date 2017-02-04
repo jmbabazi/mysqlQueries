149a3d30e46721eddc95ed8bd4982d432b6cfa13
James Mbabazi <jmbabazi@pih.org>
2017-02-04 09:44:00 -0500

    Create smeat_res_with_names.sql

---
+++ smeat_res_with_names.sql
@@ -0,0 +1,32 @@
+SELECT 
+    *
+FROM
+    (SELECT DISTINCT
+        (pi.identifier) AS 'EMR ID'
+    FROM
+        patient_identifier pi
+    JOIN obs ON pi.patient_id = obs.person_id
+    ORDER BY person_id) EMR_ID,
+    (SELECT 
+        DATE(obs_datetime) AS 'Date of Sample collection',
+            (SELECT 
+                    name
+                FROM
+                    concept_name cn
+                WHERE
+                    obs.value_coded = cn.concept_id
+                        AND cn.locale = 'es'
+                        AND cn.concept_name_type = 'SHORT'
+                        AND cn.voided = 0
+                GROUP BY person_id) AS 'Smear results'
+    FROM
+        obs
+    WHERE
+        (concept_id = 1093 AND value_coded = 248)
+            OR (concept_id = 1093 AND value_coded = 1825)
+            OR (concept_id = 1093 AND value_coded = 1826)
+            OR (concept_id = 1093 AND value_coded = 1095)
+            OR (concept_id = 1093 AND value_coded = 1096)
+            OR (concept_id = 1093 AND value_coded = 1097)
+            OR (concept_id = 1093 AND value_coded = 1092)
+    ORDER BY person_id) SMEAR;
