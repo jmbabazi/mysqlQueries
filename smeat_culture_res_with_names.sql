d0f7a6abac89822d5de5426b78586aa461835b22
James Mbabazi <jmbabazi@pih.org>
2017-02-04 10:16:22 -0500

    Update and rename smeat_res_with_names.sql to smeat_culture_res_with_names.sql

--- smeat_culture_res_with_names.sql
+++ smeat_culture_res_with_names.sql
@@ -29,4 +29,25 @@ FROM
             OR (concept_id = 1093 AND value_coded = 1096)
             OR (concept_id = 1093 AND value_coded = 1097)
             OR (concept_id = 1093 AND value_coded = 1092)
-    ORDER BY person_id) SMEAR;
+    ORDER BY person_id) SMEAR,
+    (SELECT 
+        DATE(obs_datetime),
+            (SELECT 
+                    name
+                FROM
+                    concept_name cn
+                WHERE
+                    obs.value_coded = cn.concept_id
+                        AND cn.locale = 'es'
+                        AND cn.concept_name_type = 'SHORT'
+                        AND cn.voided = 0
+                GROUP BY person_id order by person_id) AS 'Culture results'
+    FROM
+        obs
+    WHERE
+        (concept_id = 1138 AND value_coded = 1134)
+            OR (concept_id = 1138 AND value_coded = 1135)
+            OR (concept_id = 1138 AND value_coded = 1136)
+            OR (concept_id = 1138 AND value_coded = 1137)
+            OR (concept_id = 1138 AND value_coded = 246)
+     ) CULTURE;
