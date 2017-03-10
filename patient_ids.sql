5d6154b4a72d7c7036834a134ce27d1f22c24847
James Mbabazi <jmbabazi@pih.org>
2017-03-10 15:53:31 -0500

    Update patient_ids.sql

--- patient_ids.sql
+++ patient_ids.sql
@@ -3,11 +3,14 @@ SELECT
       pi.patient_id AS 'Patient ID', 
       pi.identifier AS 'EMR ID', 
       ppa.value_reference as 'Regnum' 
-from patient_identifier pi 
+from 
+      patient_identifier pi 
 join 
-      patient_program pp on 
+      patient_program pp 
+            on 
       pi.patient_id=pp.patient_id 
 join  
-      patient_program_attribute ppa on 
+      patient_program_attribute ppa 
+            on 
       pp.patient_program_id=ppa.patient_program_id and attribute_type_id=2 
 order by pi.identifier ASC;
