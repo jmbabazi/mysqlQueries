4ac74e661843b65e0cd06be5b1ba507d55e63c6b
James Mbabazi <jmbabazi@pih.org>
2017-06-26 13:01:09 -0400

    This table has coded audiometry values.

---
+++ audiometry_coded_values_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE oldResults (
+    id VARCHAR(255),
+    d_aud_test_done_old DATE,
+    t_asses_old VARCHAR(255),
+    l_1000_old VARCHAR(255),
+    l_2000_old VARCHAR(255),
+    l_4000_old VARCHAR(255),
+    l_8000_old VARCHAR(255),
+    r_1000_old VARCHAR(255),
+    r_2000_old VARCHAR(255),
+    r_4000_old VARCHAR(255),
+    r_8000_old VARCHAR(255)
+);
