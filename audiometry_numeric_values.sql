1b336a959fa3598c28ffd432614d7aeb32edfbbe
James Mbabazi <jmbabazi@pih.org>
2017-06-26 13:04:18 -0400

    The table stores numeric values fro audiomentry

---
+++ audiometry_numeric_values.sql
@@ -0,0 +1,13 @@
+CREATE TABLE newResults (
+    id VARCHAR(255),
+    d_aud_test_done_new DATE,
+    t_asses_new INT(11),
+    l_1000_new INT(11),
+    l_2000_new INT(11),
+    l_4000_new INT(11),
+    l_8000_new INT(11),
+    r_1000_new INT(11),
+    r_2000_new INT(11),
+    r_4000_new INT(11),
+    r_8000_new INT(11)
+);
